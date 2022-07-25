9216f94b008913e1a9da34ecb44d97de0a954cbb
Jassimkabir <jassim@hamon.in>
2022-07-25 13:46:06 +0530

    Adds date in note card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Home />
+      {/* <Login/> */}
     </div>
   );
 }
