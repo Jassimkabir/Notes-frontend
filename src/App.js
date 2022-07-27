d64f679144e841aacc740c0e17d32e4af405c350
Jassimkabir <jassim@hamon.in>
2022-07-27 18:39:07 +0530

    Adds basic routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const user = true;
   return (
-    <div>
-      {/* <Home /> */}
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
+        <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
