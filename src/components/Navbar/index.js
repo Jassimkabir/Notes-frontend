afe2e02921197c9ed98b7c3d12d32708f1440968
Jassimkabir <jassim@hamon.in>
2022-08-19 17:04:05 +0530

    Adds onImageError fuction

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,17 @@
 // import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 
 function Navbar({ setAddNote, logoutCard, setLogoutCard, user }) {
   const firstName = user.name.split(' ');
   const firstLetter = user.name.split('');
+
+  const [avatar, setAvatar] = useState(true);
+
+  const onImageError = () => {
+    setAvatar(false);
+  };
 
   return (
     <div className='navbarContainer d-flex justify-content-between align-items-center'>
@@ -23,14 +29,15 @@ function Navbar({ setAddNote, logoutCard, setLogoutCard, user }) {
             className='profileButton d-flex align-items-center'
             onClick={() => setLogoutCard(!logoutCard)}
           >
-            {user.avatar ? (
+            {avatar ? (
               <img
                 src={user.avatar}
                 className='profilePicture me-2'
                 alt='Profile'
+                onError={() => onImageError()}
               />
             ) : (
-              <button className='altProfileDiv d-flex align-items-center justify-content-center'>
+              <button className='altProfileDiv d-flex align-items-center justify-content-center me-2'>
                 <div className='firstLetter'>
                   {firstLetter[0]}
                   {firstLetter[1]}
