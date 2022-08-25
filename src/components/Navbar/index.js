f23697f7460ae3bb473c5f2605fdbf968e882c63
Jassimkabir <jassim@hamon.in>
2022-08-25 18:03:04 +0530

    Adds optimizely in frontend by browser specific

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,14 @@
 import React, { useState } from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
+import {
+  createInstance,
+  OptimizelyFeature,
+  OptimizelyProvider,
+  withOptimizely,
+  useDecision,
+} from '@optimizely/react-sdk';
+import { browserName, browserVersion } from 'react-device-detect';
 
 function Navbar({ setAddNote, logoutCard, setLogoutCard, user }) {
   const firstName = user.name.split(' ');
@@ -9,46 +17,67 @@ function Navbar({ setAddNote, logoutCard, setLogoutCard, user }) {
 
   const [avatar, setAvatar] = useState(true);
 
+  const optimizely = createInstance({
+    sdkKey: 'QLrDLe4wZnWctqWPt2BLs',
+  });
+
+  console.log('Optimizely Client', optimizely);
+
   const onImageError = () => {
     setAvatar(false);
   };
 
+  console.log(`${browserName}`);
+
   return (
-    <div className='navbarContainer d-flex justify-content-between align-items-center'>
-      <div className='logo'>
-        <Link to='/home' className='text-white logo'>
-          Notes
-        </Link>
-      </div>
-      <div className='containerRight d-flex align-items-center'>
-        <button className='btn btn-primary' onClick={() => setAddNote(true)}>
-          Add Note
-        </button>
-        <div className='profileDiv'>
-          <button
-            className='profileButton d-flex align-items-center'
-            onClick={() => setLogoutCard(!logoutCard)}
-          >
-            {avatar ? (
-              <img
-                src={user.avatar}
-                className='profilePicture me-2'
-                alt='Profile'
-                onError={() => onImageError()}
-              />
-            ) : (
-              <button className='altProfileDiv d-flex align-items-center justify-content-center me-2'>
-                <div className='firstLetter'>
-                  {firstLetter[0]}
-                  {firstLetter[1]}
-                </div>
-              </button>
-            )}
-            <span className='profileName'>{firstName[0]}</span>
+    <OptimizelyProvider
+      optimizely={optimizely}
+      user={{
+        id: user.id,
+        attributes: { browser_name: browserName },
+      }}
+    >
+      <div className='navbarContainer d-flex justify-content-between align-items-center'>
+        <div className='logo'>
+          <Link to='/home' className='text-white logo'>
+            Notes
+          </Link>
+        </div>
+        <div className='containerRight d-flex align-items-center'>
+          <OptimizelyFeature feature='test_flag'>
+            {(enabled) =>
+              enabled && <button className='btn btn-primary'>FireFox</button>
+            }
+          </OptimizelyFeature>
+          <button className='btn btn-primary' onClick={() => setAddNote(true)}>
+            Add Note
           </button>
+          <div className='profileDiv'>
+            <button
+              className='profileButton d-flex align-items-center'
+              onClick={() => setLogoutCard(!logoutCard)}
+            >
+              {avatar ? (
+                <img
+                  src={user.avatar}
+                  className='profilePicture me-2'
+                  alt='Profile'
+                  onError={() => onImageError()}
+                />
+              ) : (
+                <button className='altProfileDiv d-flex align-items-center justify-content-center me-2'>
+                  <div className='firstLetter'>
+                    {firstLetter[0]}
+                    {firstLetter[1]}
+                  </div>
+                </button>
+              )}
+              <span className='profileName'>{firstName[0]}</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </OptimizelyProvider>
   );
 }
 
