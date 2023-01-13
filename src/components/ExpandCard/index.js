cc0278d3d20d3ee471270210dacabfcb49077db9
Jassimkabir <jassim@hamon.in>
2023-01-13 10:51:39 +0530

    Changes design for expand note

--- src/components/ExpandCard/index.js
+++ src/components/ExpandCard/index.js
@@ -22,40 +22,45 @@ const ExpandCard = observer(({ setExpandCard, noteDetails }) => {
 
   return (
     <div className='expandContainer'>
-      <div className='expandContents'>
-        <div className='expandHead d-flex justify-content-between align-items-center'>
-          <input
-            className='w-100 noteTitle'
-            type='text'
-            placeholder='Title'
-            defaultValue={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <button onClick={() => setExpandCard(false)} className='closeButton'>
-            <i class='fa-solid fa-circle-xmark'></i>
+      <div className='expandCard'>
+        <div className='expandContents'>
+          <div className='expandHead d-flex justify-content-between align-items-center'>
+            <input
+              className='w-100 noteTitle'
+              type='text'
+              placeholder='Title'
+              defaultValue={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button
+              onClick={() => setExpandCard(false)}
+              className='closeButton'
+            >
+              <i class='fa-solid fa-circle-xmark'></i>
+            </button>
+          </div>
+          <div className='expandBody my-4'>
+            <textarea
+              cols='30'
+              rows='100'
+              placeholder='Start Typing'
+              defaultValue={description}
+              onChange={(e) => setDescription(e.target.value)}
+              type='text'
+              className='textArea w-100'
+            ></textarea>
+          </div>
+        </div>
+        {(description && description !== noteDetails?.description) ||
+        (title && title !== noteDetails?.title) ? (
+          <button
+            onClick={() => onUpdate()}
+            className='btn btn-primary updateButton'
+          >
+            Update
           </button>
-        </div>
-        <div className='expandBody my-4'>
-          <textarea
-            cols='30'
-            rows='100'
-            placeholder='Start Typing'
-            defaultValue={description}
-            onChange={(e) => setDescription(e.target.value)}
-            type='text'
-            className='textArea w-100'
-          ></textarea>
-        </div>
+        ) : null}
       </div>
-      {(description && description !== noteDetails?.description) ||
-      (title && title !== noteDetails?.title) ? (
-        <button
-          onClick={() => onUpdate()}
-          className='btn btn-primary updateButton'
-        >
-          Update
-        </button>
-      ) : null}
     </div>
   );
 });
